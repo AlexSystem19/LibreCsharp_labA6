5b98a778a4cffc2c4798ce0825a77bda259b409d
Hugo Gutierrez <analistafuncional@SERVOSA.PE>
2024-06-26 18:55:33 -0500

    buscar lista

--- NETa6/Program.cs
+++ NETa6/Program.cs
@@ -68,5 +68,32 @@ class Program
 
     //}
 
+    static void Main()
+    {
+        var a1 = new Estudiante() { nombre = "MARIA LUISA SAFRA", edad = 29, dni = 45451111,calificacion=19.63 };                   // creamos variables que sean compatibles con la lista en base a ello le pasamos los valoes segun sus campos de la clase asociada
+        var a2 = new Estudiante() { nombre = "NORMA PAIVA", edad = 36, dni = 11111111, calificacion = 12.48};
+        var a3 = new Estudiante() { nombre = "HUGO GUTIERREZ", edad = 33, dni = 98765432, calificacion = 13.63 };
+        var a4 = new Estudiante() { nombre = "NORMA PURIZACA", edad = 43, dni = 87654321, calificacion = 17.11 };
+        var a5 = new Estudiante() { nombre = "FABRIZIO GUTIERREZ", edad = 21, dni = 12345678, calificacion = 15.22 };
+        var a6 = new Estudiante() { nombre = "JOAQUIN GUTIERREZ", edad = 23, dni = 45113816, calificacion = 16.55 };
 
+        var school = new School(); // instanciamos el constructor de esa manera hereda sus metodos para ser llamados
+        school.addStudents(a1);     // al metodo constructor llamamos sus metodos que tiene disponible y le enviamo al que es de tipo añadir estudiante que recibe un argumento de tiop estudiante y la variable son del tipo estudiante el meotodo tiene el proceso de añadirlo en la lista instanciada de tipo estudiante como tal la variable es compatible con la lista
+        school.addStudents(a2);     // aplicamos el metodo ADDSTUDENTS de la clase school que ya fue instanciada en una del mismo nombre
+        school.addStudents(a3);
+        school.addStudents(a4);
+        school.addStudents(a5);
+        school.addStudents(a6);
+
+        bool valor = false;
+
+        do
+        {
+            Console.WriteLine("Ingresa el Dni a buscar");
+            int dni = Int32.Parse(Console.ReadLine()); // parsamos el numero de string a int y le pasamos al ametodo que pode un int con el nombre de argumento "dni" este al ingresar al metodo desencadenar todas las tareas las que inlcuye validar y imprimir desde la clase como tal el moeoto pide un argumento en base a ello todo se ejecuta pero tener en cuenta su tipo de ingreso
+            valor = school.buscarPorNombre(dni);// ingresa el dni en una parte del metodo devuelve un true como tal el valor final se cambiaria a true ya que se a iniciado con false ver el valor declarado antes del do el bool valor = false
+        }
+        while (valor); // al ser un TRUE termina y esperamos una tecla
+        Console.ReadKey(); // funcion que pide una tecla ya que no tiene nada despues termina la ejecucion
+    }
 }
---
+++ NETa6/School.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETa6
+{
+    public class School
+    {
+        private List<Estudiante> students;
+
+        public School()
+        {
+            students = new List<Estudiante>();                // instanciamos la lista de tipo ESTUDIANTE (clase de 4 propiedades) en un variable de nombre STUDENTS.
+        }
+
+        
+
+        //añadir estudiante a la lista STUDENT de tipo ESTUDIANTE
+
+        public void addStudents(Estudiante nuevoestudiante)   // creamos un metodo que pida un argumento de tipo Estudiante y de nombre Nuevo_estudiante
+        {
+            students.Add(nuevoestudiante);                    // a la instancia de la lista de tipo Estudiante aplicamos la funcion ADD y como pide un valor le pasamos el argumento del metodo
+        }
+
+
+        //buscar estudiante
+
+        public bool buscarPorNombre(int dni)               //cremmos un metodo que pida un argumento de tipo string de nombre NAME
+        {
+            bool encontrado = false;                           // declaramos variables en false que servira para terminar el do while
+            int i = 0;                                         // contado para avanzar en la lista que se tiene instanciada en STUDENTS
+
+            while (encontrado == false && i < students.Count)
+            {
+                if (students[i].dni.Equals(dni))
+                {
+                    encontrado = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+                if(encontrado)
+                {
+                    Console.WriteLine("Nombre :" + students[i].nombre+"\n"
+                        +"Edad :" + students[i].edad+"\n"
+                        +"Dni : "+ students[i].dni+"\n"
+                        +"Calificacion : " + students[i].calificacion);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("No se encontro estudiante, reintente");
+                    return true;
+                }
+            }
+        }
+    }
+
